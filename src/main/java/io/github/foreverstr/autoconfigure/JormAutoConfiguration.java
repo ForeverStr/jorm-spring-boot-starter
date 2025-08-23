e59ddb27ff61992124fe0e0f4fb3fb9da9f3cc5f
duyujie <wy1903265502@163.com>
2025-08-23 19:33:11 +0800

    依赖真实数据库测试

--- src/main/java/io/github/foreverstr/autoconfigure/JormAutoConfiguration.java
+++ src/main/java/io/github/foreverstr/autoconfigure/JormAutoConfiguration.java
@@ -16,7 +16,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
@@ -52,6 +54,25 @@ public class JormAutoConfiguration {
         Jorm.setDataSource(dataSource);
         return dataSource;
     }
+
+    @Bean
+    @Primary // 设置为主要的 DataSource，确保其他组件也使用事务感知的数据源
+    public DataSource jormDataSource(DataSource dataSource) {
+        return new TransactionAwareDataSourceProxy(dataSource);
+    }
+
+    @PostConstruct
+    public void init() {
+        // 获取事务感知的 DataSource 并设置给 JORM
+        DataSource ds = applicationContext.getBean("jormDataSource", DataSource.class);
+        Jorm.setDataSource(ds);
+
+        // 初始化缓存管理器
+        SecondLevelCache cache = applicationContext.getBean(SecondLevelCache.class);
+        CacheManager.setSecondLevelCache(cache);
+        CacheManager.setCacheEnabled(cacheProperties.isEnabled() && !(cache instanceof NoOpSecondLevelCache));
+    }
+
     @Bean
     @ConditionalOnBean(RedisTemplate.class)
     @ConditionalOnProperty(prefix = "jorm.cache.redis", name = "enabled", havingValue = "true")
